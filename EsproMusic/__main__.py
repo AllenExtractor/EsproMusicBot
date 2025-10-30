178eeaec0927d0df0ce25075e2ec029da62c75e9
AllenExtractor <dayarmustfa@gmail.com>
2025-10-30 09:52:12 +0530

    Update __main__.py

--- EsproMusic/__main__.py
+++ EsproMusic/__main__.py
@@ -43,7 +43,7 @@ async def init():
         await Ritik.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("EsproMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\\channel.\n\nStopping Bot..."
         )
         exit()
     except:
