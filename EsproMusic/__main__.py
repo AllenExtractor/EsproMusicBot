9b18025fe2628f3c88d874e88a98ff5407964b64
AllenExtractor <dayarmustfa@gmail.com>
2025-10-30 09:28:00 +0530

    Update __main__.py

--- EsproMusic/__main__.py
+++ EsproMusic/__main__.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 
 from pyrogram import idle
-from pytgcalls.exceptions import NoActiveGroupCall
+from ntgcalls.exceptions import NoActiveGroupCall
 
 import config
 from EsproMusic import LOGGER, app, userbot
